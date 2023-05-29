8bb3256c079842a83f81daa1d9f54b3cd8338b1d
khoind <khoind@rikkeisoft.com>
2023-05-29 17:18:58 +0700

    Fix bug

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import mongoose from "mongoose";
-
-// import posts from "./routers/posts.js";
+import posts from "./routers/posts.js";
 
 const PORT = process.env.PORT || 5000;
 
@@ -15,9 +14,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true, limit: "30mb" }));
 app.use(cors());
 
-app.use("/posts", (req, res) => {
-  res.json({ message: "Posts" });
-});
+app.use("/posts", posts);
 app.use("/", (req, res) => {
   res.json({ message: "Hello" });
 });
