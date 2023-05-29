14ec4a9f1e9fbcf6de597c7c2926cefde194b35f
duckhoi-UET <khoinguyen2000bg@gmail.com>
2023-05-30 00:38:51 +0700

    try

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import mongoose from "mongoose";
 import posts from "./routers/posts.js";
+import { PostsModel } from "./models/PostsModel.js";
 
 const PORT = process.env.PORT || 5000;
 
@@ -15,8 +16,13 @@ app.use(bodyParser.urlencoded({ extended: true, limit: "30mb" }));
 app.use(cors());
 
 app.use("/posts", posts);
-app.use("/", (req, res) => {
-  res.json({ message: "Hello" });
+app.use("/", async (req, res) => {
+  try {
+    const posts = await PostsModel.find();
+    res.status(200).json(posts);
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
 });
 
 mongoose
