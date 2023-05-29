9bd19f85d85fccab620b36538e64725fc4d6ca0a
khoind <khoind@rikkeisoft.com>
2023-05-29 17:16:11 +0700

    Test

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import mongoose from "mongoose";
 
-import posts from "./routers/posts.js";
+// import posts from "./routers/posts.js";
 
 const PORT = process.env.PORT || 5000;
 
@@ -15,7 +15,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true, limit: "30mb" }));
 app.use(cors());
 
-app.use("/posts", posts);
+app.use("/posts", (req, res) => {
+  res.json({ message: "Posts" });
+});
 app.use("/", (req, res) => {
   res.json({ message: "Hello" });
 });
