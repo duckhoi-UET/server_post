5ef9bcc3f845f24583baaf5c28b312c9403ac8c6
khoind <khoind@rikkeisoft.com>
2023-05-31 14:54:37 +0700

    Fix api me

--- middleware/checkToken.js
+++ middleware/checkToken.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 
 export const checkToken = (req, res, next) => {
   try {
-    const token = req.cookies.token;
+    const token = req.headers.authorization.split(" ")[1];
     const response = jwt.verify(token, "token");
     if (response) {
       req.data = response;
