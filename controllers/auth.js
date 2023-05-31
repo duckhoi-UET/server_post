4aaa8e122b2d9be86f18e8de080d59ea14d59e00
khoind <khoind@rikkeisoft.com>
2023-05-31 14:50:04 +0700

    Fix set cookie

--- controllers/auth.js
+++ controllers/auth.js
@@ -10,10 +10,8 @@ export const login = async (req, res) => {
     });
     if (response) {
       const token = jwt.sign({ response }, "token");
-      res
-        .cookie("token", token)
-        .status(200)
-        .json({ message: "Success", token: token });
+      res.cookie("token", token, { httpOnly: true });
+      res.status(200).json({ message: "Success", token: token });
     } else {
       res.status(500).json({ message: "Login Failed" });
     }
