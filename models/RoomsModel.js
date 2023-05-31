84b01f62372e24f7f5f3913633b7b00f60e3347b
khoind <khoind@rikkeisoft.com>
2023-05-31 17:22:47 +0700

    add rooms model

---
+++ models/RoomsModel.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const schema = new mongoose.Schema(
+  {
+    name: { type: String, require: true },
+    price: { type: String, require: true },
+    discount: { type: String, require: true, default: "0" },
+    attachment: String,
+    persons: {
+      type: Number,
+      default: 0,
+    },
+    status: { type: String },
+  },
+  {
+    timestamps: true,
+    collection: "rooms",
+  }
+);
+
+export const RoomsModel = mongoose.model("Rooms", schema);
